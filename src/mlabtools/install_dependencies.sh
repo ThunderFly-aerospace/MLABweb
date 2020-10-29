41bff51d51eacf3b49b9a322b4a084220535bd9c
Roman Dvorak <romandvorak@mlab.cz>
2020-10-30 00:02:13 +0100

    clean repos

--- src/mlabtools/install_dependencies.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install -y python-tornado python-mysqldb python-requests-oauthlib python-pip python3-pip python-pymongo python-qrcode python3-qrcode
-sudo pip install requests requests_oauthlib glob2 gitpython six w3lib xhtml2pdf --upgrade
